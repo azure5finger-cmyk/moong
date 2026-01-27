53ec2a99ee60416c908c8581fd4dbd68920f27c6
Ryu Do Hee <rnrnzhs099@gmail.com>
2026-01-27 13:45:07 +0900

    뭉프로젝트

--- users/models.py
+++ users/models.py
@@ -1,34 +1,34 @@
-# users/models.py
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    """커스텀 User 모델"""
     
-    # AbstractUser 기본 제공 필드들:
-    # username, password, email, first_name, last_name
-    # is_staff, is_active, is_superuser, date_joined, last_login, email
-    
-    # 추가 커스텀 필드들
-    nick_name = models.CharField(max_length=50, verbose_name='닉네임')
+    # 기본 프로필 정보
+    nick_name = models.CharField(
+        max_length=50, 
+        unique=True, 
+        verbose_name='닉네임'
+    )
     profile_image = models.ImageField(
         upload_to='profile_images/%Y/%m/%d/',
+        blank=True, 
+        null=True,  
         verbose_name='프로필 이미지'
     )
+    bio = models.TextField(
+        max_length=500,
+        blank=True,
+        null=True,
+        verbose_name='자기소개'
+    )
     
-    location = models.ForeignKey('locations.Location',
-                                on_delete=models.SET_NULL,
-                                null=True,
-                                blank=True,
-                                related_name='user_location',
-                                help_text="기본 활동 지역",
-                                db_column='location_id',
-                                verbose_name='주소'
-                                )
-    
-    ddomoong = models.IntegerField(default=0, verbose_name='또뭉(좋아요 수)')
-    phone = models.CharField(max_length=20, null=True, blank=True, verbose_name='전화번호')
+    # 개인 정보
+    phone = models.CharField(
+        max_length=20, 
+        null=True, 
+        blank=True, 
+        verbose_name='전화번호'
+    )
     gender = models.CharField(
         max_length=1,
         choices=[('M', '남성'), ('F', '여성'), ('O', '기타')],
@@ -36,38 +36,52 @@ class User(AbstractUser):
         blank=True,
         verbose_name='성별'
     )
+    gender_visible = models.BooleanField(
+        default=True,
+        verbose_name='성별 공개'
+    )
     
+    # 위치 정보
+    location = models.ForeignKey(
+        'locations.Location',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='users',  
+        verbose_name='주 활동 지역'
+    )
+    
+    # 활동 정보
+    ddomoong = models.IntegerField(
+        default=0, 
+        verbose_name='또뭉(좋아요 수)'
+    )
+    
+    # Django admin 페이지를 한글로 보기 위해
     class Meta:
         verbose_name = '사용자'
         verbose_name_plural = '사용자'
-        db_table = 'users'  # 테이블명을 'users'로 지정
+        db_table = 'users'
     
+    # 닉네임 우선
     def __str__(self):
-        return self.username if self.username else self.nick_name
+        return self.nick_name or self.username
     
     def increase_ddomoong(self):
         """또뭉 증가"""
         self.ddomoong += 1
-        self.save()
+        self.save(update_fields=['ddomoong']) 
     
     def decrease_ddomoong(self):
         """또뭉 감소"""
         if self.ddomoong > 0:
             self.ddomoong -= 1
-            self.save()
-
+            self.save(update_fields=['ddomoong']) 
     
-    # 추가할 필드
-    bio = models.TextField(
-        max_length=500,
-        blank=True,
-        null=True,
-        verbose_name='자기소개'
-    )
-    gender_visible = models.BooleanField(
-        default=True,
-        verbose_name='성별 공개'
-    )
+
+
+
+
 
     
 
