c2e4be4281236a91b69ab9b7439842056bf1ccda
CHIPMUNK <azure5finger@gmail.com>
2026-01-27 21:14:59 +0900

    로그인 로그아웃 추가

--- users/models.py
+++ users/models.py
@@ -93,6 +93,12 @@ class User(AbstractUser):
             self.ddomoong -= 1
             self.save(update_fields=['ddomoong'])
 
+
+
+    email = models.EmailField(unique = True,
+                              blank=False,
+                              help_text="이메일 주소")
+
     
 
 
