90bc60df7474a24b132419011e79a63b781e34c6
Ryu Do Hee <rnrnzhs099@gmail.com>
2026-01-28 11:55:45 +0900

    프로젝트 moong step4-1

--- locations/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from . import views
-
-urlpatterns = [
-    re_path(r"^sido/", views.get_sido),
-    re_path(r"^sigungu/", views.get_sigungu),
-    re_path(r"^eupmyeondong/", views.get_eupmyeondong),
-]
